325c1bca10c79657b5c1b1cf4857e2b037305eaa
Alex Corcoran <alex.p.corcoran@gmail.com>
2025-06-12 22:31:11 -0500

    Update transform when dragging

--- src/pages/Consist.tsx
+++ src/pages/Consist.tsx
@@ -104,7 +104,7 @@ function SortableItem({ id, children }: { id: string, children: React.ReactNode 
     <div
       ref={setNodeRef}
       style={{
-        transform: CSS.Transform.toString(transform),
+        transform: CSS.Translate.toString(transform),
         transition,
         opacity: isDragging ? 0.7 : 1,
         marginBottom: 8,
