34cb5cddca9f3e999bb6b8efe4006ede78b8728e
Alex Corcoran <alex.p.corcoran@gmail.com>
2025-06-20 11:44:06 -0500

    Format & add flatcar

--- src/types/ConsistItem.ts
+++ src/types/ConsistItem.ts
@@ -1,25 +1,25 @@
 export type AnyConsistItem = StaticConsistItem | JobItem;
 
 export interface ConsistItem {
-    id: string;
+  id: string;
 }
 
 export interface StaticConsistItem extends ConsistItem {
-    isStatic: true;
-    is_on: boolean;
-    can_run: boolean;
+  isStatic: true;
+  is_on: boolean;
+  can_run: boolean;
 }
 
 export interface JobItem extends ConsistItem {
-    weight: number;
-    length: number;
-    start_location: string;
-    end_location: string;
-    bonus_time_limit: number;
-    bonus_time_elapsed: number;
-    status: JobStatus;
-    end_timestamp?: EpochTimeStamp;
-    type: JobType;
+  weight: number;
+  length: number;
+  start_location: string;
+  end_location: string;
+  bonus_time_limit: number;
+  bonus_time_elapsed: number;
+  status: JobStatus;
+  end_timestamp?: EpochTimeStamp;
+  type: JobType;
 }
 
 export enum JobStatus {
@@ -29,7 +29,7 @@ export enum JobStatus {
 }
 
 export enum JobType {
-    Freight = "frieght",
-    Shunting = "shunting",
-    Logistics = "logistics",
+  Freight = "frieght",
+  Shunting = "shunting",
+  Logistics = "logistics",
 }
